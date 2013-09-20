7205fdcc09fa72472afe21f3e443dae11b520016
Nelson Elhage <nelhage@nelhage.com>
2013-09-19 20:45:18 -0400

    Import some initial ELPA packages.

---
+++ elpa/auctex-11.87/auctex-pkg.el
@@ -0,0 +1,1 @@
+(define-package "auctex" "11.87" "Integrated environment for *TeX*" nil :url "http://www.gnu.org/software/auctex/")
--- site/auctex-11.84/preview/auto.el
+++
@@ -1,4 +0,0 @@
-
-(add-to-list 'load-path (expand-file-name "auctex" (file-name-directory load-file-name)))
-(defvar preview-datadir (expand-file-name "auctex" (file-name-directory load-file-name)))
-
