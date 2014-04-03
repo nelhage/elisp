e3f41044a5fd1c85aa183c77d46cda131963ef54
Nelson Elhage <nelhage@nelhage.com>
2014-04-03 11:06:56 -0700

    go-eldoc

---
+++ elpa/go-eldoc-20140307.1711/go-eldoc-autoloads.el
@@ -0,0 +1,29 @@
+;;; go-eldoc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (go-eldoc-setup) "go-eldoc" "go-eldoc.el" (21309
+;;;;;;  41783 253184 301000))
+;;; Generated autoloads from go-eldoc.el
+
+(autoload 'go-eldoc-setup "go-eldoc" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("go-eldoc-pkg.el") (21309 41783 270372
+;;;;;;  62000))
+
+;;;***
+
+(provide 'go-eldoc-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-eldoc-autoloads.el ends here
