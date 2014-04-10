cda4614b7704d9ba9749c66e8b2fa2070936bd67
Nelson Elhage <nelhage@nelhage.com>
2014-04-10 10:33:23 -0700

    auto-complete-mode -> company-mode

---
+++ elpa/company-go-20140221.607/company-go-autoloads.el
@@ -0,0 +1,29 @@
+;;; company-go-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (company-go) "company-go" "company-go.el" (21318
+;;;;;;  54021 980192 543000))
+;;; Generated autoloads from company-go.el
+
+(autoload 'company-go "company-go" "\
+
+
+\(fn COMMAND &optional ARG &rest IGNORED)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("company-go-pkg.el") (21318 54021 999365
+;;;;;;  746000))
+
+;;;***
+
+(provide 'company-go-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; company-go-autoloads.el ends here
---
+++ elpa/company-go-20140221.607/company-go-pkg.el
@@ -0,0 +1,1 @@
+(define-package "company-go" "20140221.607" "company-mode backend for Go (using gocode)" (quote ((company "0.6.12"))))
