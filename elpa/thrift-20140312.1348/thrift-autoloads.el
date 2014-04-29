29a02318941c5b9e4d5ff08950bd45cf49885924
Nelson Elhage <nelhage@nelhage.com>
2014-04-29 16:45:38 -0700

    thrift-mode

---
+++ elpa/thrift-20140312.1348/thrift-autoloads.el
@@ -0,0 +1,30 @@
+;;; thrift-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (thrift-mode) "thrift" "thrift.el" (21327 9642
+;;;;;;  895621 474000))
+;;; Generated autoloads from thrift.el
+
+(add-to-list 'auto-mode-alist '("\\.thrift\\'" . thrift-mode))
+
+(autoload 'thrift-mode "thrift" "\
+Mode for editing Thrift files.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("thrift-pkg.el") (21327 9642 927497 481000))
+
+;;;***
+
+(provide 'thrift-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; thrift-autoloads.el ends here
---
+++ elpa/thrift-20140312.1348/thrift-pkg.el
@@ -0,0 +1,1 @@
+(define-package "thrift" "20140312.1348" "Major mode for Apache Thrift files" (quote nil))
