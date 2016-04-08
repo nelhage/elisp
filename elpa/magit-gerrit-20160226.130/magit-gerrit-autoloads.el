72b6c3bbb91e7561cfb089a2e946b24a8f0d9f72
Nelson Elhage <nelhage@nelhage.com>
2016-04-08 16:26:37 -0700

    bump magit

---
+++ elpa/magit-gerrit-20160226.130/magit-gerrit-autoloads.el
@@ -0,0 +1,16 @@
+;;; magit-gerrit-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("magit-gerrit.el") (22277 41094 448369
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magit-gerrit-autoloads.el ends here
---
+++ elpa/magit-gerrit-20160226.130/magit-gerrit-pkg.el
@@ -0,0 +1,1 @@
+(define-package "magit-gerrit" "20160226.130" "Magit plugin for Gerrit Code Review" '((magit "2.3.1")) :url "https://github.com/terranpro/magit-gerrit")
