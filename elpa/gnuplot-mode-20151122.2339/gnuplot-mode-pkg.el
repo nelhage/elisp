dcb038d6157a6a39fa6b35d316aa3fc3f348179c
Nelson Elhage <nelhage@nelhage.com>
2017-03-22 16:43:57 -0700

    elpa updates

---
+++ elpa/gnuplot-mode-20151122.2339/gnuplot-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gnuplot-mode" "20151122.2339" "Major mode for editing gnuplot scripts" 'nil :url "https://github.com/mkmcc/gnuplot" :keywords '("gnuplot" "plotting"))
