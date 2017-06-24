b04b8833f0c85c130aa122ad075828968aaf7c61
Nelson Elhage <nelhage@nelhage.com>
2017-06-24 16:01:52 -0700

    rust, company, racer

---
+++ elpa/company-racer-20160722.1658/company-racer-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-racer" "20160722.1658" "Company integration for racer" '((emacs "24") (cl-lib "0.5") (company "0.8.0") (deferred "0.3.1") (dash "2.12.0")) :commit "c2afd3d989ec2bca7dac094b684063a1922905f6" :url "https://github.com/emacs-pe/company-racer" :keywords '("convenience"))
