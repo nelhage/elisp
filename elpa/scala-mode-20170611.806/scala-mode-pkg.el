f610d16a6a7f21f4f7690164ef1f4df65eea34dc
Nelson Elhage <nelhage@nelhage.com>
2017-07-13 13:40:55 -0700

    elpa scala-mode

---
+++ elpa/scala-mode-20170611.806/scala-mode-pkg.el
@@ -0,0 +1,5 @@
+(define-package "scala-mode" "20170611.806" "Major mode for editing Scala" 'nil :url "https://github.com/ensime/emacs-scala-mode" :keywords
+  '("languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
