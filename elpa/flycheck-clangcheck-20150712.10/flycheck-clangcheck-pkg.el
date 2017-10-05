b6226f6d988e131acbbd66e9cc911a1c07f490b5
Nelson Elhage <nelhage@nelhage.com>
2017-10-05 09:28:22 -0700

    clangcheck

---
+++ elpa/flycheck-clangcheck-20150712.10/flycheck-clangcheck-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-clangcheck" "20150712.10" "A Flycheck checker difinition for ClangCheck." '((cl-lib "0.5") (seq "1.7") (flycheck "0.17")) :commit "24a9424c484420073a24443a829fd5779752362b" :url "https://github.com/kumar8600/flycheck-clangcheck")
