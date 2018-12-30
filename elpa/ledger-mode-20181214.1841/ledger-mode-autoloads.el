45d1cb939d55b62120f2fd7b3323f2c728f14686
Nelson Elhage <nelhage@nelhage.com>
2018-12-30 21:19:42 +0000

    ledger

---
+++ elpa/ledger-mode-20181214.1841/ledger-mode-autoloads.el
@@ -0,0 +1,66 @@
+;;; ledger-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "ledger-flymake" "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-flymake.el"
+;;;;;;  "54aa777fc5af92a9ff228ca9d04abf78")
+;;; Generated autoloads from ../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-flymake.el
+
+(autoload 'ledger-flymake-enable "ledger-flymake" "\
+Enable `flymake-mode' in `ledger-mode' buffers.
+
+Don't enable flymake if flycheck is on and flycheck-ledger is
+available.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil "ledger-mode" "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-mode.el"
+;;;;;;  "75f454df8c44f08d6a82d2573492e72e")
+;;; Generated autoloads from ../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-mode.el
+
+(autoload 'ledger-mode "ledger-mode" "\
+A mode for editing ledger data files.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.ledger\\'" . ledger-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-check.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-commodities.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-complete.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-context.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-exec.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-flymake.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-fontify.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-fonts.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-init.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-mode-autoloads.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-mode-pkg.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-mode.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-navigate.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-occur.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-post.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-reconcile.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-regex.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-report.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-schedule.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-sort.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-state.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-test.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-texi.el"
+;;;;;;  "../../../.emacs.d/elpa/ledger-mode-20181214.1841/ledger-xact.el")
+;;;;;;  (23593 13923 524636 701000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ledger-mode-autoloads.el ends here
