58858a98f858e9f8f0fa3b7191275bdd28f597a4
Nelson Elhage <nelhage@nelhage.com>
2019-02-18 22:11:57 +0000

    Bump company-go

---
+++ elpa/company-go-20190203.19/company-go-autoloads.el
@@ -0,0 +1,28 @@
+;;; company-go-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "company-go" "../../../.emacs.d/elpa/company-go-20190203.19/company-go.el"
+;;;;;;  "39e0c209dde9f9210d901d7293d7449f")
+;;; Generated autoloads from ../../../.emacs.d/elpa/company-go-20190203.19/company-go.el
+
+(autoload 'company-go "company-go" "\
+
+
+\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/company-go-20190203.19/company-go-autoloads.el"
+;;;;;;  "../../../.emacs.d/elpa/company-go-20190203.19/company-go.el")
+;;;;;;  (23659 11669 438772 827000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; company-go-autoloads.el ends here
