8dcf9487a87e6cca18213282eb5574aba1873ea2
Nelson Elhage <nelhage@nelhage.com>
2019-03-25 09:54:51 -0700

    we're trying helm now

---
+++ elpa/helm-git-files-20141212.1317/helm-git-files-autoloads.el
@@ -0,0 +1,57 @@
+;;; helm-git-files-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "helm-git-files" "../../../.emacs.d/elpa/helm-git-files-20141212.1317/helm-git-files.el"
+;;;;;;  "ff04d986f73a4c7ca86c5f4236ec6521")
+;;; Generated autoloads from ../../../.emacs.d/elpa/helm-git-files-20141212.1317/helm-git-files.el
+
+(autoload 'helm-git-files:git-p "helm-git-files" "\
+
+
+\(fn &optional ROOT)" nil nil)
+
+(defvar helm-git-files:modified-source nil)
+
+(defvar helm-git-files:untracked-source nil)
+
+(defvar helm-git-files:all-source nil)
+
+(autoload 'helm-git-files:submodule-sources "helm-git-files" "\
+
+
+\(fn KINDS &optional ROOT)" nil nil)
+
+(autoload 'helm-git-files "helm-git-files" "\
+`helm' for opening files managed by Git.
+
+\(fn)" t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "helm-git-files"
+;;;;;;  "../../../.emacs.d/elpa/helm-git-files-20141212.1317/helm-git-files.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../.emacs.d/elpa/helm-git-files-20141212.1317/helm-git-files.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-git-files" '("helm-git-files:")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/helm-git-files-20141212.1317/helm-git-files-autoloads.el"
+;;;;;;  "../../../.emacs.d/elpa/helm-git-files-20141212.1317/helm-git-files.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-git-files-autoloads.el ends here
---
+++ elpa/helm-git-files-20141212.1317/helm-git-files-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-git-files" "20141212.1317" "helm for git files" '((helm "1.5.9")) :commit "43193960774069369ac6964bbf7c026900206fa8" :keywords '("helm" "git") :authors '(("INA Lintaro <tarao.gnn at gmail.com>") ("TAKAGI Kentaro <kentaro0910_at_gmail.com>")) :maintainer '("INA Lintaro <tarao.gnn at gmail.com>"))
