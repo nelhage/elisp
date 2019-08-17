3ddd5e5aaf1f72d3b80420509effd27b33b180f5
Nelson Elhage <nelhage@nelhage.com>
2019-08-16 20:07:36 -0700

    company-lsp

---
+++ elpa/company-lsp-20190612.1553/company-lsp-autoloads.el
@@ -0,0 +1,30 @@
+;;; company-lsp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "company-lsp" "../../../.emacs.d/elpa/company-lsp-20190612.1553/company-lsp.el"
+;;;;;;  "ccaaebf8ed86670e35888d3c5594fc7a")
+;;; Generated autoloads from ../../../.emacs.d/elpa/company-lsp-20190612.1553/company-lsp.el
+
+(autoload 'company-lsp "company-lsp" "\
+Define a company backend for lsp-mode.
+
+See the documentation of `company-backends' for COMMAND and ARG.
+
+\(fn COMMAND &optional ARG &rest _)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/company-lsp-20190612.1553/company-lsp-autoloads.el"
+;;;;;;  "../../../.emacs.d/elpa/company-lsp-20190612.1553/company-lsp.el")
+;;;;;;  (23895 28516 429930 855000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; company-lsp-autoloads.el ends here
