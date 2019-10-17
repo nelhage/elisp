4c007448c299695ba680d1dd6412db7ae71ea112
Nelson Elhage <nelhage@nelhage.com>
2019-10-17 13:30:52 +0200

    elpa gardening

---
+++ elpa/prettier-js-20180109.726/prettier-js-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "prettier-js" "20180109.726" "Minor mode to format JS code on file save" 'nil :commit "e9b73e81d3e1642aec682195f127a42dfb0b5774" :keywords '("convenience" "wp" "edit" "js") :authors '(("James Long and contributors")) :maintainer '("James Long and contributors") :url "https://github.com/prettier/prettier-emacs")
