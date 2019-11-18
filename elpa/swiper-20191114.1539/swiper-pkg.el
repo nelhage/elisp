5975bf5d3c8fbe72705bdef7589fbf9e368a334f
Nelson Elhage <nelhage@nelhage.com>
2019-11-18 14:57:55 -0800

    Upgrade ELPA

---
+++ elpa/swiper-20191114.1539/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20191114.1539" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "a7ae5f0486b1fb5c9c9c3fa8c570ddbdce61a7b2" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
---
+++ elpa/transient-20191117.1213/transient-autoloads.el
@@ -0,0 +1,30 @@
+;;; transient-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads "actual autoloads are elsewhere" "transient" "../../../.emacs.d/elpa/transient-20191117.1213/transient.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../.emacs.d/elpa/transient-20191117.1213/transient.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "transient" '("transient-" "post-transient-hook" "current-transient-" "define-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/transient-20191117.1213/transient-autoloads.el"
+;;;;;;  "../../../.emacs.d/elpa/transient-20191117.1213/transient-pkg.el"
+;;;;;;  "../../../.emacs.d/elpa/transient-20191117.1213/transient.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; transient-autoloads.el ends here
