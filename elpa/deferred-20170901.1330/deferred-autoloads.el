6b8b9691897c79551fdf0e5b11f0c879b082114e
Nelson Elhage <nelhage@nelhage.com>
2020-04-05 17:08:47 -0700

    Remove racer

--- elpa/deferred-20170901.1330/deferred-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; deferred-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads "actual autoloads are elsewhere" "deferred" "../../../.emacs.d/elpa/deferred-20170901.1330/deferred.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../.emacs.d/elpa/deferred-20170901.1330/deferred.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "deferred" '("deferred:")))
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../.emacs.d/elpa/deferred-20170901.1330/deferred-autoloads.el"
-;;;;;;  "../../../.emacs.d/elpa/deferred-20170901.1330/deferred.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; deferred-autoloads.el ends here
