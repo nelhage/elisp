5ba5209d1f01376f4853865491d3088ebff45697
Nelson Elhage <nelhage@nelhage.com>
2020-05-27 10:01:51 -0700

    No more go-eldoc

--- elpa/go-eldoc-20170305.1427/go-eldoc-autoloads.el
+++
@@ -1,40 +0,0 @@
-;;; go-eldoc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "go-eldoc" "../../../.emacs.d/elpa/go-eldoc-20170305.1427/go-eldoc.el"
-;;;;;;  "3e92495ebb90a0715278df5f83ae0099")
-;;; Generated autoloads from ../../../.emacs.d/elpa/go-eldoc-20170305.1427/go-eldoc.el
-
-(autoload 'go-eldoc-setup "go-eldoc" "\
-Set up eldoc function and enable eldoc-mode.
-
-\(fn)" t nil)
-
-;;;### (autoloads "actual autoloads are elsewhere" "go-eldoc" "../../../.emacs.d/elpa/go-eldoc-20170305.1427/go-eldoc.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../.emacs.d/elpa/go-eldoc-20170305.1427/go-eldoc.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "go-eldoc" '("go-eldoc-")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../.emacs.d/elpa/go-eldoc-20170305.1427/go-eldoc-autoloads.el"
-;;;;;;  "../../../.emacs.d/elpa/go-eldoc-20170305.1427/go-eldoc.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; go-eldoc-autoloads.el ends here
