e41716063c53119241c40a9eb84148e24c39d062
Nelson Elhage <nelhage@nelhage.com>
2020-05-29 16:32:14 -0700

    csv-mode

---
+++ elpa/csv-20161113.1510/csv-autoloads.el
@@ -0,0 +1,29 @@
+;;; csv-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads "actual autoloads are elsewhere" "csv" "../../../.emacs.d/elpa/csv-20161113.1510/csv.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../.emacs.d/elpa/csv-20161113.1510/csv.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "csv" '("csv-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/csv-20161113.1510/csv-autoloads.el"
+;;;;;;  "../../../.emacs.d/elpa/csv-20161113.1510/csv.el") (0 0 0
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; csv-autoloads.el ends here
