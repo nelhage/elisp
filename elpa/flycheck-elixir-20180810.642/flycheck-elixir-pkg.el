9947ed3f34e862937261f3b211b5f968ea76b17b
Nelson Elhage <nelhage@nelhage.com>
2020-10-13 10:55:43 -0700

    elixir, erlang

---
+++ elpa/flycheck-elixir-20180810.642/flycheck-elixir-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/nelhage/.emacs.d/elpa/flycheck-elixir-20180810.642/flycheck-elixir.el  -*- no-byte-compile: t -*-
+(define-package "flycheck-elixir" "20180810.642" "Support Elixir in flycheck" '((flycheck "0.25")) :commit "11998d7e3e63a33453e934d25b3673f7c558e579" :authors '(("Lorenzo Bolla" . "lbolla@gmail.com")) :maintainer '("Lorenzo Bolla" . "lbolla@gmail.com"))
