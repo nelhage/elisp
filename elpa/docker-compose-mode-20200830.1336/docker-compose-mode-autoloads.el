352dc552f3b208e842a2ff415eadf4a7335165c2
Nelson Elhage <nelhage@nelhage.com>
2020-12-10 10:34:17 -0800

    Docker modes

---
+++ elpa/docker-compose-mode-20200830.1336/docker-compose-mode-autoloads.el
@@ -0,0 +1,36 @@
+;;; docker-compose-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "../../../.emacs.d/elpa/docker-compose-mode-20200830.1336/docker-compose-mode"
+;;;;;;  "../../../.emacs.d/elpa/docker-compose-mode-20200830.1336/docker-compose-mode.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../.emacs.d/elpa/docker-compose-mode-20200830.1336/docker-compose-mode.el
+
+(autoload 'docker-compose-mode "../../../.emacs.d/elpa/docker-compose-mode-20200830.1336/docker-compose-mode" "\
+Major mode to edit docker-compose files.
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("docker-compose[^/]*\\.ya?ml\\'" . docker-compose-mode))
+
+(register-definition-prefixes "../../../.emacs.d/elpa/docker-compose-mode-20200830.1336/docker-compose-mode" '("docker-compose-"))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/docker-compose-mode-20200830.1336/docker-compose-mode-autoloads.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; docker-compose-mode-autoloads.el ends here
