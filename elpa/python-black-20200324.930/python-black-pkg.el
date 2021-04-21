bdc1cfa89d6b859ebab24787412b4b6666dbac4d
Nelson Elhage <nelhage@anthropic.com>
2021-04-20 19:11:22 -0700

    Python black

---
+++ elpa/python-black-20200324.930/python-black-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from python-black.el  -*- no-byte-compile: t -*-
+(define-package "python-black" "20200324.930" "Reformat Python using python-black" '((emacs "25") (dash "2.16.0") (reformatter "0.3")) :commit "a11ca73f6dfcdc125d27ff184496d66bdbd71326" :authors '(("wouter bolsterlee" . "wouter@bolsterl.ee")) :maintainer '("wouter bolsterlee" . "wouter@bolsterl.ee") :keywords '("languages") :url "https://github.com/wbolster/emacs-python-black")
