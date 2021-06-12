2ad2092f4d2365a092637e873aeee120b401ff91
Nelson Elhage <nelhage@nelhage.com>
2021-06-12 11:56:14 -0700

    elpa

---
+++ elpa/ninja-mode-20181024.1439/ninja-mode-autoloads.el
@@ -0,0 +1,36 @@
+;;; ninja-mode-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "../../../.emacs.d/elpa/ninja-mode-20181024.1439/ninja-mode"
+;;;;;;  "../../../.emacs.d/elpa/ninja-mode-20181024.1439/ninja-mode.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../.emacs.d/elpa/ninja-mode-20181024.1439/ninja-mode.el
+
+(autoload 'ninja-mode "../../../.emacs.d/elpa/ninja-mode-20181024.1439/ninja-mode" "\
+
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.ninja$" . ninja-mode))
+
+(register-definition-prefixes "../../../.emacs.d/elpa/ninja-mode-20181024.1439/ninja-mode" '("ninja-"))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/ninja-mode-20181024.1439/ninja-mode-autoloads.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ninja-mode-autoloads.el ends here
---
+++ elpa/ninja-mode-20181024.1439/ninja-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ninja-mode.el  -*- no-byte-compile: t -*-
+(define-package "ninja-mode" "20181024.1439" "Major mode for editing .ninja files" '((emacs "24")) :commit "68f971460bb21abd59dfe6253e35a55c3e1cfc64")
