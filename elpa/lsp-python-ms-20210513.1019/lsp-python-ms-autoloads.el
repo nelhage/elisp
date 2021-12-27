f6f2225d23e658f877b38ec3582f88aa011d0cf3
Nelson Elhage <nelhage@nelhage.com>
2021-12-27 08:40:29 -0800

    python lsp-mode

---
+++ elpa/lsp-python-ms-20210513.1019/lsp-python-ms-autoloads.el
@@ -0,0 +1,35 @@
+;;; lsp-python-ms-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "../../../.emacs.d/elpa/lsp-python-ms-20210513.1019/lsp-python-ms"
+;;;;;;  "../../../.emacs.d/elpa/lsp-python-ms-20210513.1019/lsp-python-ms.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../.emacs.d/elpa/lsp-python-ms-20210513.1019/lsp-python-ms.el
+
+(autoload 'lsp-python-ms-update-server "../../../.emacs.d/elpa/lsp-python-ms-20210513.1019/lsp-python-ms" "\
+Update Microsoft Python Language Server.
+
+On Windows, if the server is running, the updating will fail.
+After stopping or killing the process, retry to update." t nil)
+
+(register-definition-prefixes "../../../.emacs.d/elpa/lsp-python-ms-20210513.1019/lsp-python-ms" '("lsp-python-ms-"))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/lsp-python-ms-20210513.1019/lsp-python-ms-autoloads.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lsp-python-ms-autoloads.el ends here
