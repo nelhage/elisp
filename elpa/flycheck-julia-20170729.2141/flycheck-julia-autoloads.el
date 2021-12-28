4f5475b572d7949f5f380361cc178859f7bf77f0
Nelson Elhage <nelhage@nelhage.com>
2021-12-28 11:03:01 -0600

    julia

---
+++ elpa/flycheck-julia-20170729.2141/flycheck-julia-autoloads.el
@@ -0,0 +1,41 @@
+;;; flycheck-julia-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flycheck-julia" "../../../.emacs.d/elpa/flycheck-julia-20170729.2141/flycheck-julia.el"
+;;;;;;  "35ff2f7a5f0a0fecc39b1ef41e7f1e90")
+;;; Generated autoloads from ../../../.emacs.d/elpa/flycheck-julia-20170729.2141/flycheck-julia.el
+
+(autoload 'flycheck-julia-setup "flycheck-julia" "\
+Setup Flycheck Julia.
+
+Add `flycheck-julia' to `flycheck-checkers'." t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "flycheck-julia"
+;;;;;;  "../../../.emacs.d/elpa/flycheck-julia-20170729.2141/flycheck-julia.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../.emacs.d/elpa/flycheck-julia-20170729.2141/flycheck-julia.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flycheck-julia" '("flycheck-julia-")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/flycheck-julia-20170729.2141/flycheck-julia-autoloads.el"
+;;;;;;  "../../../.emacs.d/elpa/flycheck-julia-20170729.2141/flycheck-julia.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flycheck-julia-autoloads.el ends here
---
+++ elpa/julia-repl-20210913.1256/julia-repl-autoloads.el
@@ -0,0 +1,52 @@
+;;; julia-repl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "julia-repl" "../../../.emacs.d/elpa/julia-repl-20210913.1256/julia-repl.el"
+;;;;;;  "ee81f0eabb1c46cbb4d5671007948003")
+;;; Generated autoloads from ../../../.emacs.d/elpa/julia-repl-20210913.1256/julia-repl.el
+
+(autoload 'julia-repl "julia-repl" "\
+Raise the Julia REPL inferior buffer, creating one if it does not exist.
+
+This is the standard entry point for using this package." t nil)
+
+(autoload 'julia-repl-mode "julia-repl" "\
+Minor mode for interacting with a Julia REPL running inside a term.
+
+If called interactively, enable Julia-Repl mode if ARG is
+positive, and disable it if ARG is zero or negative.  If called
+from Lisp, also enable the mode if ARG is omitted or nil, and
+toggle it if ARG is `toggle'; disable the mode otherwise.
+
+\\{julia-repl-mode-map}
+
+\(fn &optional ARG)" t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "julia-repl" "../../../.emacs.d/elpa/julia-repl-20210913.1256/julia-repl.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../.emacs.d/elpa/julia-repl-20210913.1256/julia-repl.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "julia-repl" '("julia-repl-")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/julia-repl-20210913.1256/julia-repl-autoloads.el"
+;;;;;;  "../../../.emacs.d/elpa/julia-repl-20210913.1256/julia-repl.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; julia-repl-autoloads.el ends here
