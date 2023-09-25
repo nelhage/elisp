129a022309aec4c04739f3d45719c6e89f3b440b
Nelson Elhage <nelhage@nelhage.com>
2023-09-25 09:09:46 +0200

    idk man stuff

---
+++ elpa/jinja2-mode-20220117.807/jinja2-mode-autoloads.el
@@ -0,0 +1,39 @@
+;;; jinja2-mode-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "../../../.emacs.d/elpa/jinja2-mode-20220117.807/jinja2-mode"
+;;;;;;  "../../../.emacs.d/elpa/jinja2-mode-20220117.807/jinja2-mode.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../.emacs.d/elpa/jinja2-mode-20220117.807/jinja2-mode.el
+
+(autoload 'jinja2-mode "../../../.emacs.d/elpa/jinja2-mode-20220117.807/jinja2-mode" "\
+Major mode for editing jinja2 files
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.jinja2\\'" . jinja2-mode))
+
+(add-to-list 'auto-mode-alist '("\\.j2\\'" . jinja2-mode))
+
+(register-definition-prefixes "../../../.emacs.d/elpa/jinja2-mode-20220117.807/jinja2-mode" '("jinja2-" "sgml-indent-line-num"))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../.emacs.d/elpa/jinja2-mode-20220117.807/jinja2-mode-autoloads.el"
+;;;;;;  "../../../.emacs.d/elpa/jinja2-mode-20220117.807/jinja2-mode-pkg.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; jinja2-mode-autoloads.el ends here
