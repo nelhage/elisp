a173e2747398191bc111281a98563f636137e823
Nelson Elhage <nelhage@nelhage.com>
2024-02-22 10:06:16 -0800

    bump js2-mode

---
+++ elpa/js2-mode-20231225.1150/js2-mode-pkg.el
@@ -0,0 +1,17 @@
+(define-package "js2-mode" "20231225.1150" "Improved JavaScript editing mode"
+  '((emacs "24.1")
+    (cl-lib "0.5"))
+  :commit "ca0af32eea0281322a9ce268d98f01fbb88bbb7a" :authors
+  '(("Steve Yegge" . "steve.yegge@gmail.com")
+    ("mooz" . "stillpedant@gmail.com")
+    ("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainers
+  '(("Steve Yegge" . "steve.yegge@gmail.com"))
+  :maintainer
+  '("Steve Yegge" . "steve.yegge@gmail.com")
+  :keywords
+  '("languages" "javascript")
+  :url "https://github.com/mooz/js2-mode/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
