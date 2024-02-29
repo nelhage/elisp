87a0da2d60dfb9ead5bd5ff0ba7e484534a00fda
Nelson Elhage <nelhage@anthropic.com>
2024-02-29 08:23:23 -0800

    Finally kill off site/

--- site/psgml-1.3.2/psgml-sysdep.el
+++
@@ -1,9 +0,0 @@
-
-(provide 'psgml-sysdep)
-
-(require 'psgml)
-(cond
- (sgml-running-lucid
-  (require 'psgml-lucid))
- (t
-  (require 'psgml-other)))
--- site/vm-7.19/vm.el
+++
@@ -1,7 +0,0 @@
-(defun vm-its-such-a-cruel-world ()
-   (require 'vm-version)
-   (require 'vm-startup)
-   (require 'vm-vars)
-   (require 'vm-autoload))
-(vm-its-such-a-cruel-world)
-(fmakunbound 'vm-its-such-a-cruel-world)
