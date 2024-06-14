e817bfdda0b1510834375f92eef4a460f529f1b5
Nelson Elhage <nelhage@nelhage.com>
2024-06-14 10:34:46 -0700

    Try web-mode for HTML

---
+++ elpa/web-mode-20240301.937/web-mode-pkg.el
@@ -0,0 +1,14 @@
+(define-package "web-mode" "20240301.937" "major mode for editing web templates"
+  '((emacs "23.1"))
+  :commit "10f1269ab1007ab3d5fe018c83bb82c979617bfd" :authors
+  '(("François-Xavier Bois"))
+  :maintainers
+  '(("François-Xavier Bois" . "fxbois@gmail.com"))
+  :maintainer
+  '("François-Xavier Bois" . "fxbois@gmail.com")
+  :keywords
+  '("languages")
+  :url "https://web-mode.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
