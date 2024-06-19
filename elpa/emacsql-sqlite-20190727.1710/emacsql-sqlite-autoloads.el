07d1db44a53295cfc1cc0a20f90d33f0e211672a
Nelson Elhage <nelhage@nelhage.com>
2024-06-19 13:09:20 -0700

    defunct

--- elpa/emacsql-sqlite-20190727.1710/emacsql-sqlite-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; emacsql-sqlite-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads "actual autoloads are elsewhere" "emacsql-sqlite"
-;;;;;;  "../../../.emacs.d/elpa/emacsql-sqlite-20190727.1710/emacsql-sqlite.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../.emacs.d/elpa/emacsql-sqlite-20190727.1710/emacsql-sqlite.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "emacsql-sqlite" '("emacsql-sqlite-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../.emacs.d/elpa/emacsql-sqlite-20190727.1710/emacsql-sqlite-autoloads.el"
-;;;;;;  "../../../.emacs.d/elpa/emacsql-sqlite-20190727.1710/emacsql-sqlite-pkg.el"
-;;;;;;  "../../../.emacs.d/elpa/emacsql-sqlite-20190727.1710/emacsql-sqlite.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; emacsql-sqlite-autoloads.el ends here
