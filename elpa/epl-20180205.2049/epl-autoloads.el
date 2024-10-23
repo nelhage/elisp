8f7ffa61be2fabc56c822c0340dae60f0464b94a
Nelson Elhage <nelhage@anthropic.com>
2024-10-23 13:44:31 -0700

    Nuke forge

--- elpa/epl-20180205.2049/epl-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads "actual autoloads are elsewhere" "epl" "../../../.emacs.d/elpa/epl-20180205.2049/epl.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../.emacs.d/elpa/epl-20180205.2049/epl.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "epl" '("epl-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../.emacs.d/elpa/epl-20180205.2049/epl-autoloads.el"
-;;;;;;  "../../../.emacs.d/elpa/epl-20180205.2049/epl.el") (0 0 0
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; epl-autoloads.el ends here
