c86e87607b87d1916ab31c59cdd0a45423f3016e
Nelson Elhage <nelhage@nelhage.com>
2025-01-01 17:36:14 -0800

    try window-number from elpa

---
+++ elpa/window-number-20170801.151/window-number-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "window-number" "20170801.151"
+  "Select windows by numbers."
+  ()
+  :url "https://github.com/nikolas/window-number"
+  :commit "d41722de646ffeb3f70d26e4a86a5a1ba5c6be87"
+  :revdesc "d41722de646f"
+  :keywords '("windows")
+  :authors '(("Johann Myrkraverk Oskarsson" . "myrkraverk@users.sourceforge.net"))
+  :maintainers '(("Nik Nyby" . "niknyby@riseup.net")
+                 ("Johann Myrkraverk Oskarsson" . "myrkraverk@users.sourceforge.net")
+                 ("Andy Stewart" . "lazycat.manatee@gmail.com")))
