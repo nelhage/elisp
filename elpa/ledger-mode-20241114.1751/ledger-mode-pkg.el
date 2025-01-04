753cdc35e10f0a2a44094ee8f28dd41678a36911
Nelson Elhage <nelhage@nelhage.com>
2025-01-03 17:58:06 -0800

    Add ELPA ledger-mode

---
+++ elpa/ledger-mode-20241114.1751/ledger-mode-pkg.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ledger-mode" "20241114.1751"
+  "Helper code for use with the \"ledger\" command-line tool."
+  '((emacs "25.1"))
+  :url "https://github.com/ledger/ledger-mode"
+  :commit "15b7d29f2539f9e9671ab3c062bd5165e5b80ae8"
+  :revdesc "15b7d29f2539")
