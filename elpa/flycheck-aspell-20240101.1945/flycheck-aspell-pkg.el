68af2fac2ff6a0a71c7a212ae999b9986e2a18d1
Nelson Elhage <nelhage@nelhage.com>
2025-01-13 17:33:23 -0800

    spellchecking in Markdown

---
+++ elpa/flycheck-aspell-20240101.1945/flycheck-aspell-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "flycheck-aspell" "20240101.1945"
+  "Aspell checker for flycheck."
+  '((flycheck "28.0")
+    (emacs    "25.1"))
+  :url "https://github.com/leotaku/flycheck-aspell"
+  :commit "aa73874abc60a43fcf615af9bdd85d3008bfe687"
+  :revdesc "aa73874abc60"
+  :keywords '("wp" "flycheck" "spell" "aspell")
+  :authors '(("Leo Gaskin" . "leo.gaskin@le0.gs"))
+  :maintainers '(("Leo Gaskin" . "leo.gaskin@le0.gs")))
