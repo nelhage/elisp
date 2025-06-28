27d1e1e896fd620a0ab9ba594bd30a079c56bfd2
Nelson Elhage <nelhage@nelhage.com>
2025-06-28 10:54:12 -0700

    reorganize custom

---
+++ custom.el
@@ -0,0 +1,44 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(confirm-kill-processes nil)
+ '(custom-safe-themes
+   '("e223120256455daba01b6c68510b48fac813acab05c314510e47aea377b23634"
+     default))
+ '(ns-command-modifier 'meta)
+ '(package-selected-packages
+   '(ac-rtags adaptive-wrap apheleia auto-complete base16-theme
+              bison-mode caddyfile-mode chatgpt-shell clang-format
+              clojure-mode cmake-mode coffee-mode company-coq
+              company-go company-rtags conda counsel csv csv-mode
+              cuda-mode dash-at-point debian-changelog-mode
+              docker-compose-mode dockerfile-mode edit-server ein
+              elixir-mode envrc erlang exec-path-from-shell f flycheck
+              flycheck-aspell flycheck-clangcheck flycheck-elixir
+              flycheck-julia flycheck-ocaml flycheck-rust fuzzy
+              git-link gnu-elpa-keyring-update gnuplot gnuplot-mode
+              gptel graphviz-dot-mode haskell-mode helm helm-git-files
+              helm-ls-git inf-ruby isortify ivy javaimp jinja2-mode
+              js2-mode julia-mode julia-repl ledger-mode lsp-pyright
+              lsp-python-ms lsp-ui lua-mode magit-gerrit
+              magit-gh-pulls magit-popup mistty mode-line-bell
+              ninja-mode nix-mode obsidian polymode popwin prettier-js
+              py-isort python-black reformatter rubocop ruby-electric
+              ruby-mode ruby-tools rust-mode rust-mode scala-mode seq
+              svelte-mode swiper terraform-mode typescript-mode
+              verilog-mode visual-fill-column visual-fill-column
+              web-mode window-number yaml-mode zig-mode))
+ '(safe-local-variable-values
+   '((lsp-disabled-clients mspyls pylsp)
+     (lsp-pylsp-plugins-yapf-enabled . t)
+     (lsp-enabled-clients '(pylsp)) (encoding . utf-8)
+     (gofmt-command . "gofmt") (gofmt-command . "goimports"))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
