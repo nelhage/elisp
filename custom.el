3d4739453f5ce114a32a30560d814b8a3250f829
Nelson Elhage <nelhage@nelhage.com>
2025-12-05 23:04:02 +0000

    Some tweaks for remote TTY emacs.

--- custom.el
+++ custom.el
@@ -11,7 +11,7 @@
  '(package-selected-packages
    '(ac-rtags adaptive-wrap apheleia auto-complete base16-theme
               bison-mode caddyfile-mode chatgpt-shell clang-format
-              clojure-mode cmake-mode coffee-mode company-coq
+              clipetty clojure-mode cmake-mode coffee-mode company-coq
               company-go company-rtags conda counsel csv csv-mode
               cuda-mode dash-at-point debian-changelog-mode
               docker-compose-mode dockerfile-mode edit-server ein
@@ -26,13 +26,13 @@
               lsp-python-ms lsp-ui lua-mode magit-gerrit
               magit-gh-pulls magit-popup mistty mode-line-bell
               ninja-mode nix-mode polymode popwin prettier-js
-              protobuf-mode py-isort python-black reformatter rubocop ruby-electric
-              ruby-mode ruby-tools rust-mode rust-mode scala-mode seq
-              svelte-mode swiper terraform-mode typescript-mode
-              verilog-mode visual-fill-column visual-fill-column
-              web-mode window-number yaml-mode zig-mode))
- '(safe-local-variable-directories
-   '("/Users/nelhage/code/obsidian.el/"))
+              protobuf-mode py-isort python-black reformatter rubocop
+              ruby-electric ruby-mode ruby-tools rust-mode rust-mode
+              scala-mode seq svelte-mode swiper terraform-mode
+              typescript-mode verilog-mode visual-fill-column
+              visual-fill-column web-mode window-number yaml-mode
+              zig-mode))
+ '(safe-local-variable-directories '("/Users/nelhage/code/obsidian.el/"))
  '(safe-local-variable-values
    '((lsp-disabled-clients mspyls pylsp)
      (lsp-pylsp-plugins-yapf-enabled . t)
---
+++ elpa/clipetty-20200327.2241/clipetty-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "clipetty" "20200327.2241"
+  "Send every kill from a TTY frame to the system clipboard."
+  '((emacs "25.1"))
+  :url "https://github.com/spudlyo/clipetty"
+  :commit "7ee3f9c52f70f80820a8c66fb6f796d6e01dd92d"
+  :revdesc "7ee3f9c52f70"
+  :keywords '("terminals" "convenience")
+  :authors '(("Mike Hamrick" . "mikeh@muppetlabs.com"))
+  :maintainers '(("Mike Hamrick" . "mikeh@muppetlabs.com")))
