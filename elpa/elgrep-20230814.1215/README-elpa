8bb800a53149dbda8f264e73e5f22a280ee30848
Nelson Elhage <nelhage@nelhage.com>
2026-08-27 18:51:52 +0000

    Restore elgrep

---
+++ elpa/elgrep-20230814.1215/README-elpa
@@ -0,0 +1,9 @@
+Open the `elgrep-menu' via menu item "Tools" -> "Search files (Elgrep)...".
+There are menu items for the directory, the file name regexp for filtering
+and the regexp for grepping.
+Furthermore, you can also switch on recursive grep.
+
+Run M-x elgrep to search a single directory for files with file
+name matching a given regular expression for text matching a given
+regular expression.
+With prefix arg it searches the directory recursively.
---
+++ elpa/elgrep-20230814.1215/elgrep-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "elgrep" "20230814.1215"
+  "Searching files for regular expressions."
+  '((emacs "26.2")
+    (async "1.5"))
+  :url "https://github.com/TobiasZawada/elgrep"
+  :commit "329eaf2e9e994e5535c7f7fe2685ec21d8323384"
+  :revdesc "329eaf2e9e99"
+  :keywords '("tools" "matching" "files" "unix")
+  :authors '(("Tobias Zawada" . "i@tn-home.de"))
+  :maintainers '(("Tobias Zawada" . "i@tn-home.de")))
